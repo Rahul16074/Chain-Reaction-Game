edbad42ca1fe003b60d05847f3c3a69bbd3e27c3
vipul16117 <vipul16117@iiitd.ac.in>
2017-10-19 02:31:29 +0530

    Update menu.java

--- menu.java
+++ menu.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.stage.Stage;
 
 public class menu implements javafx.fxml.Initializable{
 	 @FXML
@@ -23,7 +28,7 @@ public class menu implements javafx.fxml.Initializable{
 	    private Button play_HD;
 
 	    @FXML
-	    private ComboBox<?> sel_player;
+	    private ComboBox<String> sel_player;
 
 	    @FXML
 	    private Button setting;
@@ -41,14 +46,34 @@ public class menu implements javafx.fxml.Initializable{
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-
+		sel_player.getItems().remove("Item 1");
+		sel_player.getItems().remove("Item 2");
+		sel_player.getItems().remove("Item 3");
+		sel_player.getItems().add("2");
+		sel_player.getItems().add("3");
+		sel_player.getItems().add("4");
+		sel_player.getItems().add("5");
+		sel_player.getItems().add("6");
+		sel_player.getItems().add("7");
+		sel_player.getItems().add("8");
+		
 		play.setOnAction(new EventHandler<ActionEvent>(){
 			@Override
 			public void handle (ActionEvent e){
 				Example ex=new Example();
 				ex.run();
 			}
-		});		
+		});
+		setting.setOnAction(new EventHandler<ActionEvent>(){
+
+			@Override
+			public void handle(ActionEvent event) {
+				// TODO Auto-generated method stub
+				Player_Setting p=new Player_Setting();
+				p.run();
+			}
+			
+		});
 	}
 	
 }
