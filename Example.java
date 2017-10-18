a93bceade3e03324d14c2623684940f67930c980
Rahul16074 <Rahul16074@users.noreply.github.com>
2017-10-18 20:16:35 +0530

    3

--- Example.java
+++ Example.java
@@ -1,19 +1,14 @@
 package Chain_reaction;
 
 import javafx.application.Application;
-import javafx.geometry.Pos;
+import javafx.geometry.HPos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Line;
-import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Sphere;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Example extends Application{
@@ -34,17 +29,47 @@ public class Example extends Application{
         grid.setStyle("-fx-background-color: black; -fx-grid-lines-visible: true");
         grid.setTranslateX(100);
         grid.setTranslateY(100);
-        Scene scene = new Scene(root, (6 * 100) + 100, (9 * 100) + 100, Color.BLACK);
+        Scene scene = new Scene(root, 2000,2000, Color.BLACK);
+        int check[][] = new int[9][6];
         grid.setOnMouseClicked(event ->{
+        	
         	Sphere sphere = new Sphere(20);
         	
-        	
-        	int x = (int)event.getSceneX()/100;
-        	int y = (int)event.getSceneY()/100;
-        	sphere.setTranslateX(((x + x + 1)/2.0)*100);
-        	sphere.setTranslateY(((y + y + 1)/2.0)*100);
-        	sphere.setVisible(true);
-        	root.getChildren().add(sphere);
+        	int x = (int)event.getSceneX()/100 - 1;
+        	int y = (int)event.getSceneY()/100 - 1;
+        	System.out.println(x + " " + y);
+        	if(check[y][x] == 1)
+        	{
+        		if((x!=0 || y!=0) &&  (x!=0 || y!=8) && (x!=5 || y!=0) && (x!=5 || y!=8) )
+        		{
+        			sphere.setTranslateX(10);
+        			sphere.setTranslateY(10);
+        			grid.add(sphere, x, y);
+        			check[y][x]++;
+        		}
+        		
+        	}
+        	else if(check[y][x] == 2)
+            {
+        		if(x!=0 && x!=5 && y!=0 && y!=8)
+        		{
+        			sphere.setTranslateX(40);
+        			sphere.setTranslateY(10);
+        			grid.add(sphere, x, y);
+        			check[y][x]++;
+        		}
+            }
+        	else if(check[y][x] == 0)
+        	{
+        	//System.out.println(x + " " + y);
+        	//sphere.setTranslateX(((x + x + 1)/2.0)*100);
+        	//sphere.setTranslateY(((y + y + 1)/2.0)*100);
+        	grid.add(sphere, x, y);
+        	GridPane.setHalignment(sphere, HPos.CENTER);
+        	check[y][x]++;
+        	//sphere.setVisible(true);
+        	//root.getChildren().add(sphere);
+        	}
         });
         
         primaryStage.setScene(scene);
