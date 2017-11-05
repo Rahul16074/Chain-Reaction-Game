09cf950f6a256616c41c31b16a575c9b0c2daf0c
vipul16117 <vipul16117@iiitd.ac.in>
2017-11-05 19:16:14 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 
 public class Main extends Application {
@@ -11,7 +12,10 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		this.primaryStage=primaryStage;
+		primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("icon.png")));
+		primaryStage.setTitle("CHAIN REACTION GAME");
 		try {
+			
 			AnchorPane root =FXMLLoader.load(getClass().getResource("main_menu.fxml"));
 			Scene scene=new Scene(root);
 			primaryStage.setScene(scene);
